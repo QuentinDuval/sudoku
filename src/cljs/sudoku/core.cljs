e2dfc5b482d66bc93999cbef9dbb8e923c1ff784
Duval Quentin <senmenty@gmail.com>
2016-12-07 21:41:05 +0100

    starting to look like a sudoku

--- src/cljs/sudoku/core.cljs
+++ src/cljs/sudoku/core.cljs
@@ -1,13 +1,55 @@
 (ns sudoku.core
   (:require [reagent.core :as reagent :refer [atom]]))
 
+
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(def board-width 9)
+(def board-height 9)
+
+
+(defn init-matrix
+  "Initialize the game matrix"
+  []
+  (into {}
+    (for [i (range board-width)
+          j (range board-height)]
+      [[i j] 0]
+      )))
+
+(defonce app-state
+  (atom {:matrix (init-matrix)}))
+
+
+(defn rect-cell
+  "Draw a rectangle cell on the screen"
+  [x y]
+  [:rect.cell
+   {:x (+ 0.05 x) :width 0.9
+    :y (+ 0.05 y) :height 0.9
+    :fill "none"
+    :stroke-width 0.01
+    :stroke "black"}
+   ])
+
+
+(defn render-board
+  []
+  (into
+    [:svg.board
+     {:view-box (str "0 0 " board-width " " board-height)
+      :style {:max-height "500px"}}]
+    (for [i (range board-width)
+          j (range board-height)]
+      [rect-cell i j]
+      )))
 
 (defn sudoku
   []
-  [:h1 (:text @app-state)])
+  [:div
+   [:h1 "Sudoku"]
+   [:div [render-board]]
+   ])
 
 (reagent/render
   [sudoku]
